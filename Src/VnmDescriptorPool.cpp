df07565457614c1273bf5dd3975482fbd78e3678
Matthew Rusch <tripleoverhead@yahoo.com>
2020-01-02 22:55:38 -0800

    Add samplers to pool, make pool larger

--- Src/VnmDescriptorPool.cpp
+++ Src/VnmDescriptorPool.cpp
@@ -4,17 +4,24 @@
 
 namespace Vnm
 {
+    const uint32_t defaultUniformBufferDescriptorCount = 1024;
+    const uint32_t defaultSampledImageDescriptorCount  = 1024;
+    const uint32_t defaultSamplerDescriptorCount       = 1024;
+
+    // TODO: Size
     void DescriptorPool::Create(Device& device)
     {
-        VkDescriptorPoolSize descriptorPoolSize[2] = {};
+        VkDescriptorPoolSize descriptorPoolSize[3] = {};
         descriptorPoolSize[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        descriptorPoolSize[0].descriptorCount = 1;
+        descriptorPoolSize[0].descriptorCount = defaultUniformBufferDescriptorCount;
         descriptorPoolSize[1].type = VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE;
-        descriptorPoolSize[1].descriptorCount = 1;
+        descriptorPoolSize[1].descriptorCount = defaultSampledImageDescriptorCount;
+        descriptorPoolSize[2].type = VK_DESCRIPTOR_TYPE_SAMPLER;
+        descriptorPoolSize[2].descriptorCount = defaultSamplerDescriptorCount;
 
         VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {};
         descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-        descriptorPoolCreateInfo.poolSizeCount = 2;
+        descriptorPoolCreateInfo.poolSizeCount = sizeof(descriptorPoolSize) / sizeof(descriptorPoolSize[0]);
         descriptorPoolCreateInfo.pPoolSizes = descriptorPoolSize;
         descriptorPoolCreateInfo.maxSets = 16;
 
